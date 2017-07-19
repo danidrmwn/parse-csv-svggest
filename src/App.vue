4979ded36d226e74c2ea3469dde6177efb6960a1
Juliana Bacelar <juliana.univasf@gmail.com>
2017-07-19 10:43:55 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <parse></parse>
+  </div>
+</template>
+
+<script>
+import Parse from './components/Parse'
+
+export default {
+  name: 'app',
+  components: {
+    Parse
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Parse.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="parse">
+    <h1>Parse CSV to JSON</h1>
+    <input 
+      id="fileInput"
+      type="file"
+      @change="upload">
+    <a
+      @click='save'
+      type='button'
+      download >
+      Save
+    </a>
+    <div class="body">
+      <div class="entry">
+        <textarea 
+          class="entry-result"
+          v-model='doc'
+          placeholder="Type here">
+        </textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Papa from 'papaparse'
+  import Blob from 'blob'
+  import FileSaver from 'file-saver'
+
+  export default {
+    name: 'parse',
+    data () {
+      return {
+        doc: null
+      }
+    },
+    methods: {
+      upload (e) {
+        const that = this
+        const fileToLoad = event.target.files[0]
+        const reader = new FileReader()
+        reader.onload = fileLoadedEvent => {
+          Papa.parse(fileLoadedEvent.target.result, {
+            header: true,
+            complete (results) {
+              console.log('complete', results)
+              that.doc = JSON.stringify(results.data, null, 2)
+            },
+            error (errors) {
+              console.log('error', errors)
+            }
+          })
+        }
+        reader.readAsText(fileToLoad)
+      },
+      save () {
+        const blob = new Blob([this.parseJSONtoCSV()], { type: 'text/csv' })
+        FileSaver.saveAs(blob, 'test.csv')
+      },
+      parseJSONtoCSV () {
+        return Papa.unparse(this.doc)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .body {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
+
+  .entry {
+    width: 40%;
+  }
+
+  .entry-result {
+    width: 100%;
+    height: 50vh;
+  }
+
+  .preview {
+    width: 40%;
+    text-align: left;
+  }
+
+
+</style>
